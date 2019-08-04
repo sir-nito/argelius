a5dc28f203299794cdccb924d96c1325df0c80d4
sir-nito <45302744+sir-nito@users.noreply.github.com>
2019-08-03 20:52:04 -0500

    Update comentarios.js

--- routes/comentarios.js
+++ routes/comentarios.js
@@ -4,7 +4,7 @@
 var express = require('express');
 var ComentariosController = require('../controllers/comentarios');
 var api = express.Router();
-var middleware = require('../../system-23/api/middlewares/authenticated');
+var middleware = require('../middlewares/authenticated');
 
 api.post('/enviar-comentario',middleware.ensureAuth,ComentariosController.sendComentario);
 api.get('/get-comentarios/:id',middleware.ensureAuth,ComentariosController.getComentarios);
